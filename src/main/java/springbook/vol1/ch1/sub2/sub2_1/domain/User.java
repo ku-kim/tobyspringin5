745713207b187d38f5b75ea018468854d1bbe256
“kukim” <kukim.dev@gmail.com>
2022-09-04 00:35:15 +0900

    refactor: 중복 코드 getConnection() 메서드 추출

---
+++ src/main/java/springbook/vol1/ch1/sub2/sub2_1/domain/User.java
@@ -0,0 +1,14 @@
+package springbook.vol1.ch1.sub2.sub2_1.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+	private String id;
+	private String name;
+	private String password;
+
+}
